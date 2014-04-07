5dab19ab018f625c5965f4a35fce22ed934b8ed9
tuvistavie <tuvistavie@gmail.com>
2014-04-08 02:18:34 +0900

    Update sidebar. Add logout link.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ HackathonUserSystem::Application.routes.draw do
 
   devise_for :users, skip: [:sessions]
   as :user do
-    post '/login' => 'devise/sessions#create', as: 'user_session'
+    post   '/login' => 'devise/sessions#create', as: 'user_session'
+    delete '/logout' => 'devise/sessions#destroy', as: 'destroy_user_session'
   end
 end
