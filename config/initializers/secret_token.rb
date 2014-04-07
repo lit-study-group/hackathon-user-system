2f995456c3844def598ecfff0f4595f4c4848110
Daniel Perez <tuvistavie@gmail.com>
2014-04-07 13:30:51 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackathonUserSystem::Application.config.secret_key_base = '6f38c1b2e77f279cd5416ed99ba386211e3fbe5dac20dded683f3839583f92ad574d97234411594a07194d1808028069ca54cbebe1d1578430e1e1f40f23a77a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackathonUserSystem::Application.config.session_store :cookie_store, key: '_hackathon-user-system_session'
